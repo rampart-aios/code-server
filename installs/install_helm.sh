fc28771f35c35f3fbb3b1376a12b8527a9f81ee2
Chip Lee <chiplee@gmail.com>
2024-07-18 14:54:05 +0400

    new sessions

--- installs/install_helm.sh
+++ installs/install_helm.sh
@@ -10,9 +10,12 @@ helm upgrade --install cs-chip ../ci/helm-chart -f values-base-docker.yaml -f va
 
 
 ## AIOS-ws
-helm upgrade --install cs-gpu-chip ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -n cs-gpu-chip --create-namespace
+helm upgrade --install cs-gpu-chip ../ci/helm-chart -f values-base-docker.yaml -f values-4gpu.yaml -n cs-gpu-chip --create-namespace
 helm upgrade --install cs-gpu-abdelmajid ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -n cs-gpu-abdelmajid --create-namespace
 helm upgrade --install cs-akhmed ../ci/helm-chart -f values-base-docker.yaml -f values-2gpu.yaml -n cs-akhmed --create-namespace
+helm upgrade --install cs-janine ../ci/helm-chart -f values-base-docker.yaml -n cs-janine --create-namespace
+
+
 ## ciai-ws
 helm upgrade --install cs-gpu-chip ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -n cs-gpu-chip --create-namespace
 
