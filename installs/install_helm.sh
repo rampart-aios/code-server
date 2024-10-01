349b761c427f26c45bfcc899b100f9a25d9d1340
Chip Lee <chiplee@gmail.com>
2024-10-01 15:40:03 +0400

    copy values files to chart

--- installs/install_helm.sh
+++ installs/install_helm.sh
@@ -8,17 +8,13 @@ helm upgrade --install cs-ridwan ../ci/helm-chart -f values-base-docker.yaml -f 
 helm upgrade --install cs-hanshuo ../ci/helm-chart -f values-base-docker.yaml -f values-2gpu.yaml  -f values-bright-mounts.yaml -n cs-hanshuo --create-namespace
 helm upgrade --install cs-chip ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml  -f values-bright-mounts.yaml -n cs-chip --create-namespace
 helm upgrade --install cs-akhmed ../ci/helm-chart -f values-base-docker.yaml -f values-2gpu.yaml   -f values-bright-mounts.yaml -n cs-akhmed --create-namespace
-helm upgrade --install cs-xuechi ../ci/helm-chart -f values-base-docker.yaml -f values-bright-mounts.yaml -n cs-xuechi --create-namespace
+helm upgrade --install cs-xuechi ../ci/helm-chart -f values-base-docker.yaml -f values-2gpu.yaml  -f values-bright-mounts.yaml -n cs-xuechi --create-namespace
 
 ## AIOS-ws
-helm upgrade --install cs-gpu-chip ../ci/helm-chart -f values-base-docker.yaml -f values-4gpu.yaml -n cs-gpu-chip --create-namespace
-helm upgrade --install cs-gpu-abdelmajid ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -n cs-gpu-abdelmajid --create-namespace
-helm upgrade --install cs-akhmed ../ci/helm-chart -f values-base-docker.yaml -f values-2gpu.yaml -n cs-akhmed --create-namespace
-helm upgrade --install cs-janine ../ci/helm-chart -f values-base-docker.yaml -n cs-janine --create-namespace
-helm upgrade --install cs-hanshuo ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -n cs-hanshuo --create-namespace
-helm upgrade --install cs-janine ../ci/helm-chart -f values-base-docker.yaml -n cs-janine --create-namespace
-helm upgrade --install cs-xuechi ../ci/helm-chart -f values-base-docker.yaml -n cs-xuechi --create-namespace
-
+helm upgrade --install cs-elnura ../ci/helm-chart -f values-base-docker.yaml -f values-aios-ws-mounts.yaml -f values-2gpu.yaml -n cs-elnura --create-namespace
+helm upgrade --install cs-demo ../ci/helm-chart -f values-base-docker.yaml -f values-aios-ws-mounts.yaml -f values-2gpu.yaml -n cs-demo --create-namespace \
+--set image.tag=1.0.0-9876e46
+helm upgrade --install cs-omar ../ci/helm-chart -f values-base-docker.yaml -f values-aios-ws-mounts.yaml -f values-2gpu.yaml -n cs-omar --create-namespace
 
 ## ciai-ws
 helm upgrade --install cs-gpu-chip ../ci/helm-chart -f values-base-docker.yaml -f values-1gpu.yaml -n cs-gpu-chip --create-namespace
